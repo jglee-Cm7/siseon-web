de30003acff9d7cc37190dbe499f72baabd45e4f
jglee-Cm7 <jinkyu0826@gmail.com>
2025-12-08 16:40:37 +0900

    feat: Update section height style

--- components/features/home/welcome-section.tsx
+++ components/features/home/welcome-section.tsx
@@ -1,10 +1,9 @@
-import { cn } from "@/lib/utils";
-import { ChevronRightIcon, PhoneIcon } from "lucide-react";
+import { PhoneIcon } from "lucide-react";
 import Image from "next/image";
 
 export function WelcomeSection() {
   return (
-    <section className="bg-[url('/images/background.avif')] md:max-h-[800px]">
+    <section className="bg-[url('/images/background.avif')]">
       <div className="h-full bg-white/90 px-6 py-12 md:px-30 md:py-24">
         <div className="mx-auto max-w-[1440px]">
           <div className="flex h-full flex-col gap-6 md:flex-row">
